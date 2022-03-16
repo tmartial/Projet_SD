830c1fd14482f1bd7984fcdfa0bb30212fdd649f
tmartial <94893617+tmartial@users.noreply.github.com>
2022-03-16 16:37:14 +0100

    Second commit

--- encodeur.py
+++ encodeur.py
@@ -11,7 +11,6 @@ from multiprocessing import Pool
 import os
 from PIL import Image
 from matplotlib import image
-from skimage import io
 
 path1 = "C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/img_align_celeba/img_align_celeba"
 listing = os.listdir(path1)
@@ -27,34 +26,23 @@ def decoupe_liste(liste, taille_decoupe):
         intervalle_1 = intervalle_1 + taille_decoupe
     return listing_parts 
 
-listing_parts=decoupe_liste(listing,20000)
+listing_parts=decoupe_liste(listing,100)
 print(listing_parts)
 
-nparray = np.array(listing_parts[0])
-print(nparray)
-
-for file in nparray:
-        print(file)
+for file in listing_parts[0]:
         chemin = "C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/img_align_celeba/img_align_celeba/"+file
-        im = io.imread(chemin)
-        imarray = img_to_array(im)
-        listarray.append(imarray)
+        im = image.imread(chemin)
+        listarray.append(im)
 print(listarray)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test = train_test_split (listing_parts[0], test_size=0.2, random_state=0)
+nparray = np.array(listarray)
+print(nparray)
+print(nparray.shape)
 
-
-img = array_to_img(listarray[0])
+img = array_to_img(nparray[0])
 plt.imshow(img)
 plt.show()
-print(img.size)
 
-img2 = array_to_img(listarray[1])
-plt.imshow(img2)
-plt.show()
-print(img2.size)
-
-
-print(listarray)
+from sklearn.model_selection import train_test_split
+X_train, X_test = train_test_split (nparray, test_size=0.2, random_state=0)
 
