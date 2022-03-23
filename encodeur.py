3afd33ab96545fa7a1f5f698e5f0197b4a9f0a4d
tmartial <94893617+tmartial@users.noreply.github.com>
2022-03-23 10:47:22 +0100

    Add files via upload
    Fichier avec l'encodeur:
    -Importation des images
    -Mise en place du modèle encodeur et entrainement
    -Obtention des images encodés
    -Sauvegarde du modèle encodeur et des images encodé
    MODIFICATION A FAIRE: en fonction du fichier de EMMA.M
    DOCUMENTATION A FAIRE
    
    A FAIRE/FINIR: modèle décodeur

--- encodeur.py
+++ encodeur.py
@@ -15,7 +15,8 @@ from matplotlib import image
 from keras.datasets import mnist
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose
 
-path1 = "C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/img_align_celeba/img_align_celeba"
+#1. Importation des images et transformation en numpy array
+path1 = "C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/celebA_test/celebA_test"
 listing = os.listdir(path1)
 listarray = []
 
@@ -29,42 +30,22 @@ def decoupe_liste(liste, taille_decoupe):
         intervalle_1 = intervalle_1 + taille_decoupe
     return listing_parts 
 
-listing_parts=decoupe_liste(listing,100)
-print(listing_parts)
+listing_parts=decoupe_liste(listing,500) #500 images
 
+from skimage.transform import resize
 for file in listing_parts[0]:
-        chemin = "C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/img_align_celeba/img_align_celeba/"+file
+        chemin = "C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/celebA_test/celebA_test/"+file
         im = image.imread(chemin)
-        listarray.append(im)
-print(listarray)
-
+        resized_img = resize(im,(128,128))
+        listarray.append(resized_img)
 nparray = np.array(listarray)
-print(nparray)
-print(nparray.shape)
 
-img = array_to_img(nparray[0])
-plt.imshow(img)
-plt.show()
-
+#Création de ensemble d'entrainement et de test
 from sklearn.model_selection import train_test_split
 X_train, X_test = train_test_split (nparray, test_size=0.2, random_state=0)
 
-print("X_train", X_train.shape)
-print("X_test", X_test.shape)
-
-def show_face_data(nparray, n=10, title=""):
-    plt.figure(figsize=(30, 5))
-    for i in range(n):
-        ax = plt.subplot(2,n,i+1)
-        plt.imshow(array_to_img(nparray[i]))
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-    plt.suptitle(title, fontsize = 20)
-
-show_face_data(X_train, title="X train")
-show_face_data(X_test, title="X test")
-
-input_layer = Input(shape=(218, 178, 3), name="INPUT")
+#Mise en place du modèle pour l'encodeur
+input_layer = Input(shape=(128, 128, 3), name="INPUT")
 x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_layer)
 x = MaxPooling2D((2, 2))(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
@@ -83,4 +64,59 @@ output_layer = Conv2D(3, (3, 3), padding='same', name="OUTPUT")(x)
 
 AE = Model(input_layer, output_layer)
 AE.compile(optimizer='adam', loss='mse')
-AE.summary()
+
+
+
+#Entrainement du modèle encodeur
+AE.fit(X_train, X_train,
+                epochs=2,
+                batch_size=32,
+                shuffle=True,
+                validation_data=(X_test, X_test))
+
+#Sauvegarde du modèle
+AE.save("C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/encodeur.h5")
+
+#Fonction pour obtenir le vecteur associé à l'image encodé
+def auto_encoder():
+    get_encoded_X = Model(inputs=AE.input, outputs=AE.get_layer("CODE").output)
+
+    encoded = get_encoded_X.predict(X_test)
+    encoded = encoded.reshape((len(X_test), 16*16*8))
+    reconstructed = AE.predict(X_test)
+    return encoded, reconstructed
+
+encoded, reconstructed=auto_encoder()
+
+#Sauvegarde du vecteur encodé
+np.save("C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/vecteur_encodé.npy", encoded)
+
+#Visualisation sur un ensemble d'image
+def show_face_data(nparray, n=10, title=""):
+    plt.figure(figsize=(30, 5))
+    for i in range(n):
+        ax = plt.subplot(2,n,i+1)
+        plt.imshow(array_to_img(nparray[i]))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.suptitle(title, fontsize = 20)
+    plt.show()
+
+show_face_data(X_test, title="original faces")
+show_face_data(reconstructed, title="reconstructed faces")
+
+#Mise en place de façon analogique du modèle pour le décodeur
+input_layer_decodeur = Input(shape=(16,16,8), name="INPUT")
+x_decodeur = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(input_layer_decodeur)
+x_decodeur = UpSampling2D((2, 2))(input_layer_decodeur)
+x_decodeur = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(input_layer_decodeur)
+x_decodeur = UpSampling2D((2, 2))(input_layer_decodeur)
+x_decodeur = Conv2DTranspose(16, (3, 3), activation='relu', padding='same')(input_layer_decodeur)
+x_decodeur = UpSampling2D((2,2))(input_layer_decodeur)
+output_layer_decodeur = Conv2D(3, (3, 3), padding='same', name="OUTPUT")(input_layer_decodeur)
+
+D = Model(input_layer_decodeur,output_layer_decodeur)
+D.compile(optimizer='adam', loss='mse')
+
+#Sauvegarde du modèle décodeur
+AE.save("C:/Users/toto9/OneDrive/Documents/4BIM S2/PROJET/decodeur.h5")
