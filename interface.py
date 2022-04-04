bba09a841aec0e044a7a49971cab8d62fa34f45e
androlias <otrancart@outlook.com>
2022-04-04 17:54:42 +0200

    Interface notation

--- interface.py
+++ interface.py
@@ -3,10 +3,11 @@
 ###############
 
 import tkinter as tk
+from tkinter import ttk,messagebox
 from tkinter import *
 from tkinter.messagebox import askokcancel, showinfo, WARNING
 from tkinter.filedialog import asksaveasfile
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image #(sudo) pip3 install pillow
 import os
 
 
@@ -37,6 +38,8 @@ root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 #   IDENTIFICATION   #
 ######################
 
+## Sélection par choix #########################################################################
+"""
 select = IntVar(root, value = 0)
 def on_click(number):
     print(all_buttons[number])
@@ -56,20 +59,27 @@ def on_click(number):
     for i in range(6):
         if  all_buttons[i]['highlightbackground']=='red':
             sel+=1
-            select.set(sel)
-        
+            select.set(sel)   
 
 class Portrait(Button):
     def __init__(self, master, number, image):
         Button.__init__(self, master, image=image,command=lambda: on_click(number),activebackground='#345')
-    
+ """ 
 
-# decoded_imgs[i] 
+## Variables globales ##########################################################################
+selected_lvl0 = tk.StringVar()
+selected_lvl1 = tk.StringVar()
+selected_lvl2 = tk.StringVar()
+selected_lvl3 = tk.StringVar()
+selected_lvl4 = tk.StringVar()
+selected_lvl5 = tk.StringVar()
+index = IntVar(root, value = 1)
 
+
+## Fonctions ###################################################################################
 def show_portraits():
-    global all_buttons
-    all_buttons=[]
-    print(all_buttons)
+    global all_cb
+    all_cb=[]
     number=0
     
     liste=os.listdir("./img") # Recupere le nom de tous les fichiers d'un dossier
@@ -79,27 +89,36 @@ def show_portraits():
         resize_image = Image.open(imgpath).resize((218, 178))
         img = ImageTk.PhotoImage(resize_image)  ## Chargement d'une image à partir de PIL
     
-        button = Portrait(root,number=number,image=img)
+        button=Label(root,image=img)   #button = Portrait(root,number=number,image=img)
         button.photo = img   # assign to class variable to resolve problem with bug in `PhotoImage`
         if number<3:
-            button.grid(row=1, column=number)
+            R=1
+            C=number
         else :
-            button.grid(row=2, column=number-3)
+            R=3
+            C=number-3
+
+        button.grid(row=R, column=C)
+        lvl = ttk.Combobox(root, textvariable='selected_lvl'+str(number))
+        lvl['values'] = ["not at all similar","similar","very similar"]
+        lvl['state'] = 'readonly'
+        lvl.grid(row=R+1, column=C)
         
-        all_buttons.append(button)
+        #all_buttons.append(button)
+        all_cb.append(lvl)
         
     root.grid_columnconfigure(1, weight=1)
     root.grid_columnconfigure(2, weight=1)
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(0, weight=1)
     root.grid_rowconfigure(1, weight=2)
-    root.grid_rowconfigure(2, weight=2)
-    root.grid_rowconfigure(3, weight=1)
+    root.grid_rowconfigure(2, weight=1)
+    root.grid_rowconfigure(3, weight=2)
     root.grid_rowconfigure(4, weight=1)
-    root.grid_rowconfigure(5, weight=3)
+    root.grid_rowconfigure(5, weight=1)
+    root.grid_rowconfigure(6, weight=2)
 
 
-index = IntVar(root, value = 1)
 def start():
     welcome.destroy()
     intro.destroy()
@@ -107,11 +126,20 @@ def start():
     ind=index.get()
     selection = Label(root, 
             text='Selection '+str(ind)+'/10')
-    selection.grid(row=4, column=1)
+    selection.grid(row=5, column=1)
     button_next.configure(text='Confirm selection')
-    button_next.configure(state='disabled')
+    #button_next.configure(state='disabled')
     button_next.configure(command=confirm)
 
+def notation(list):
+    i=0
+    for i in range(len(list)):
+        if list[i]=="not at all similar":
+            list[i]=0.1
+        elif list[i]=="similar":
+            list[i]=0.5
+        elif list[i]=="very similar":
+            list[i]=0.9
 
 def SaveFile(img):
     #data = [('All tyes(*.*)', '*.*')]
@@ -141,23 +169,39 @@ def end():
     )
     button_save.place(relx=0.5, rely=0.8,anchor='center')
 
-
 def confirm():
-    ind=index.get()
-    answer = askokcancel(
-        title='Confirmation',
-        message='You have selected '+str(select.get())+' image(s). Do you want to confirm your selection?',
-        icon=WARNING)
+    i=0
+    for i in range(6):
+        if all_cb[i].get()=="":
+            messagebox.showinfo(title='Warning', 
+                message='Please select a notation for each portrait',
+                icon=WARNING)
+            break
+        else:  
+            ind=index.get()
+            answer = askokcancel(
+                title='Confirmation',
+                message='Do you want to confirm your selection?',
+                icon=WARNING)
 
-    if answer:
-        ind+=1
-        selection = Label(root,text='Selection '+str(ind)+'/10')
-        selection.grid(row=4, column=1)
-        index.set(ind)
-        show_portraits()
+            if answer:
+                ind+=1
+                selection = Label(root,text='Selection '+str(ind)+'/10')
+                selection.grid(row=4, column=1)
+                index.set(ind)
 
-        if ind==3:
-            end()
+                j=0
+                all_lvl=[]
+                for j in range(6):
+                    all_lvl.append(all_cb[j].get())
+                    notation(all_lvl)
+                    all_cb[j].set("")
+                print(all_lvl)
+                show_portraits()
+
+            if ind==3:
+                end()
+            break
 
 
 ############
@@ -171,7 +215,7 @@ welcome.pack()
 welcome.place(relx=0.2, rely=0.2)
 
 intro = Label(root, 
-    text="This software allows you to create a robot portrait from an image bank.\nChoose the faces that most resemble the person concerned.\nYou can choose between 1 and 6 images at a time by clicking on it, during 10 selections.\nBe careful! You can't go back on your selection.",
+    text="This software allows you to create a robot portrait from an image bank.\nChoose a notation for each portrait using the combobox.\nYou can choose between 3 notations : 'not at all similar','similar' and 'very similar'\nThere are 10 selections with 6 portraits to note each time.\nBe careful! You can't go back on your selection.",
     font=("Helvetica", 15))
 intro.pack()
 intro.place(relx=0.5, rely=0.5,anchor='center')
